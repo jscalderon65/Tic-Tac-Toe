988c480ee6e678be3a376f03de769887e24729eb
Sebastian Calderon <jhonnseca0599@gmail.com>
2021-06-28 09:19:55 -0500

    Main objectives

---
+++ README.md
@@ -0,0 +1,15 @@
+# OBJETIVOS
+
+## -> Botón de abandonar.
+
+## -> Botón de finalizar partida.
+
+## -> Validar que usuario no pueda jugar cuando no es su turno.
+
+## -> Añadir modal explicativo
+
+## -> Desplegar en Netlify
+
+## -> Agregar README
+
+## -> Crear video explicativo
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 import { GithubOutlined } from "@ant-design/icons";
 const Navbar = () => {
-    return (
-<div className="OptionsView-navbar">
-        <div className="OptionsView-navbar-brand">Tic-tac-toe</div>
-        <div className="OptionsView-navbar-icons">
-          <GithubOutlined />
-        </div>
+  const onOpenGitHub = () =>{
+    window.open("https://github.com/jscalderon65/Tic-Tac-Toe");
+  }
+  return (
+    <div className="OptionsView-navbar">
+      <div className="OptionsView-navbar-brand">Tic-tac-toe</div>
+      <div className="OptionsView-navbar-icons">
+        <GithubOutlined onClick={onOpenGitHub}/>
       </div>
-    )
-}
+    </div>
+  );
+};
 export default Navbar;
